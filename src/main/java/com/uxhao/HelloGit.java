8aa8491c5764428f0b564506bf3fe280aa6719de
xuhao <73208206+ahaox@users.noreply.github.com>
2021-03-23 17:39:30 +0800

    第一个版本

---
+++ src/main/java/com/uxhao/HelloGit.java
@@ -0,0 +1,13 @@
+package com.uxhao;
+
+/**
+ * @author ：ahao
+ * @version : 1.0
+ * @date ：Created in 2021/3/23 17:34
+ * @description ：
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
