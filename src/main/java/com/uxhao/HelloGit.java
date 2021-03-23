2694f761e680383b6c4c69415cf25b57d5f240b1
xuhao <73208206+ahaox@users.noreply.github.com>
2021-03-23 18:12:36 +0800

    master主干添加

--- src/main/java/com/uxhao/HelloGit.java
+++ src/main/java/com/uxhao/HelloGit.java
@@ -11,5 +11,6 @@ public class HelloGit {
         System.out.println("hello git");
         System.out.println("更新1");
         System.out.println("创建分支dev");
+        System.out.println("master主干添加");
     }
 }
